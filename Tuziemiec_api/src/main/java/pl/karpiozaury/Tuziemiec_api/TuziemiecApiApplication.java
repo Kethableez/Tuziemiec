16331f6a1bccc3f27aaad00a9997663e090882b4
Kethableez <janiszyn.amadeusz@gmail.com>
2021-03-22 16:04:12 +0100

    Project created!

---
+++ Tuziemiec_api/src/main/java/pl/karpiozaury/Tuziemiec_api/TuziemiecApiApplication.java
@@ -0,0 +1,13 @@
+package pl.karpiozaury.Tuziemiec_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuziemiecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuziemiecApiApplication.class, args);
+	}
+
+}
---
+++ Tuziemiec_api/src/test/java/pl/karpiozaury/Tuziemiec_api/TuziemiecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karpiozaury.Tuziemiec_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuziemiecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
